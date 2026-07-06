c76dfb0e100fdf47fd95e78dce2d4cb3f65c1a4f
hasini-reddyy <hasinireddy1011@gmail.com>
2026-07-06 18:49:09 +0530

    Converting the given Roman value into a Number

---
+++ ARRAYS/romanToNum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char, int> roman;
+
+        roman['I'] = 1;
+        roman['V'] = 5;
+        roman['X'] = 10;
+        roman['L'] = 50;
+        roman['C'] = 100;
+        roman['D'] = 500;
+        roman['M'] = 1000;
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+        if (i + 1 < s.length() && roman[s[i]] < roman[s[i + 1]]) {
+            result -= roman[s[i]];
+        } else {
+            result += roman[s[i]];
+        }
+    }
+
+        return result;
+
+    }
+};
